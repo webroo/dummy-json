86a7cbd0c9e5272d25dbad0370123c252ff22711
Matt Sweetman <git@webroo.org>
2013-06-15 11:42:29 +0100

    Repeat helper now accepts an array as well as numeric ranges

--- dummy-json.js
+++ dummy-json.js
@@ -15,21 +15,42 @@ var randomBoolean = function() {
 var uniqueIndex;
 
 var helpers = {
-  repeat: function(count, maxCount, options) {
-    // If maxCount isn't specified then repeat the exact number of times
-    if (arguments.length === 2) {
-      options = maxCount;
-      maxCount = count;
+  repeat: function(min, max, options) {
+    // By default repeat will work with it's current context, unless it's
+    // given an array in which case it'll work with that
+    var context = this;
+    var contextIsArray = false;
+    var count = 0;
+
+    if (Array.isArray(min)) {
+      // If the helper was given an array then juggle the arguments
+      options = max;
+      context = min;
+      count = context.length;
+      contextIsArray = true;
+    } else {
+      if (arguments.length === 3) {
+        count = randomInt(min, max);
+      } else if (arguments.length === 2) {
+        // The helper can be given just one number for a fixed repeat count
+        options = max;
+        count = min;
+      }
     }
-    count = randomInt(count, maxCount);
 
     var ret = '';
     for (var i = 0; i < count; i++) {
-      ret += options.fn(this, {data: {index: i, count: count}});
+      // index and count are passed as a private variable so they don't pollute
+      // the context scope
+      ret += options.fn(
+        contextIsArray ? context[i] : context,
+        {data: {index: i, count: count}}
+      );
       // Trim whitespace left by handlebars and add commas between items
       ret = ret.trim();
       if (i < count - 1) ret += ',';
     }
+
     return ret;
   },
 
