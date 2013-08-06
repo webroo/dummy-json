0dc36f41b67d6f56326c1dee5b911dc9d5eb5866
Matt Sweetman <git@webroo.org>
2013-08-06 11:19:07 +0100

    Added first and last name helpers

--- dummy-json.js
+++ dummy-json.js
@@ -1,5 +1,10 @@
 var Handlebars = require('handlebars');
 
+var firstNames = ['Leanne','Edward','Haydee','Lyle','Shea','Curtis','Roselyn','Marcus','Lyn','Lloyd','Isabelle','Francis','Olivia','Roman','Myong','Jamie','Alexis','Vernon','Chloe','Max','Kirstie','Tyler','Katelin','Alejandro','Hannah','Gavin','Lynetta','Russell','Neida','Kurt','Dannielle','Aiden','Janett','Vaughn','Michelle','Brian','Maisha','Theo','Emma','Cedric','Jocelyn','Darrell','Grace','Ivan','Rikki','Erik','Madeleine','Rufus','Florance','Raymond','Jenette','Danny','Kathy','Michael','Layla','Rolf','Selma','Anton','Rosie','Craig','Victoria','Andy','Lorelei','Drew','Yuri','Miles','Raisa','Rico','Rosanne','Cory','Dori','Travis','Joslyn','Austin','Haley','Ian','Liza','Rickey','Susana','Stephen','Richelle','Lance','Jetta','Heath','Juliana','Rene','Madelyn','Stan','Eleanore','Jason','Alexa','Adam','Jenna','Warren','Cecilia','Benito','Elaine','Mitch','Raylene','Cyrus'];
+var lastNames = ['Flinn','Young','Milligan','Keesee','Mercer','Chapman','Zobel','Carter','Pettey','Starck','Raymond','Pullman','Drolet','Higgins','Matzen','Tindel','Winter','Charley','Schaefer','Hancock','Dampier','Garling','Verde','Lenihan','Rhymer','Pleiman','Dunham','Seabury','Goudy','Latshaw','Whitson','Cumbie','Webster','Bourquin','Connor','Rikard','Brier','Luck','Porras','Gilmore','Turner','Sprowl','Rohloff','Magby','Wallis','Mullens','Correa','Murphy','Bryd','Gamble','Castleman','Pace','Durrett','Bourne','Hottle','Oldman','Paquette','Stine','Muldoon','Smit','Finn','Kilmer','Sager','White','Friedrich','Fennell','Miers','Carroll','Freeman','Hollis','Neal','Remus','Pickering','Woodrum','Bradbury','Caffey','Tuck','Jensen','Shelly','Hyder','Krumm','Hundt','Seal','Pendergast','Kelsey','Milling','Karst','Helland','Risley','Grieve','Paschall','Coolidge','Furlough','Brandt','Cadena','Rebelo','Leath','Backer','Bickers','Cappel'];
+
+var personIndex = 0;
+
 var randomInt = function(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
@@ -80,6 +85,14 @@ var helpers = {
 
   uniqueIndex: function(options) {
     return uniqueIndex++;
+  },
+
+  firstName: function(options) {
+    return firstNames[personIndex++];
+  },
+
+  lastName: function(options) {
+    return lastNames[personIndex++];
   }
 };
 
@@ -92,8 +105,9 @@ module.exports = {
     Handlebars.Utils.extend(combinedHelpers, helpers);
     Handlebars.Utils.extend(combinedHelpers, options.helpers);
 
-    // Reset uniqueIndex on each parse
+    // Reset indexes on each parse
     uniqueIndex = 0;
+    personIndex = 0;
 
     return Handlebars.compile(string)(options.data, {helpers: combinedHelpers});
   },
