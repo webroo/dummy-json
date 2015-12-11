9521d4e3a754efe49dc7c83b01285e5e131fa9d0
Matt Sweetman <git@webroo.org>
2015-12-11 18:04:03 +0000

    Removed minimist dependency

--- dummy-json-cli.js
+++ dummy-json-cli.js
@@ -2,13 +2,13 @@
 
 var os = require('os');
 var fs = require('fs');
-var argv = require('minimist')(process.argv.slice(2));
 var dummyjson = require('./dummy-json');
 
-if (argv._ && argv._.length) {
-    // We're only reading one filename for now
-    var result = dummyjson.parse(
-        fs.readFileSync(argv._[0], {encoding: 'utf8'})
-    );
-    process.stdout.write(result + os.EOL);
+var args = process.argv.slice(2);
+
+if (args && args[0]) {
+  var result = dummyjson.parse(
+    fs.readFileSync(args[0], {encoding: 'utf8'})
+  );
+  process.stdout.write(result + os.EOL);
 }
